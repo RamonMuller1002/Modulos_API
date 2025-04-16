2f34e72a48d55a619b629545b318fc2c6c8f5851
RAMON MULLER <ramon_muller1@estudante.sesisenai.org.br>
2025-04-16 11:21:32 -0300

    Updating API

--- API/script.js
+++ API/script.js
@@ -2,19 +2,36 @@ const express = require('express');
 const app = express()  // inicializa executando a função express()
 const PORT = 8000 // reserva a porta para rodar o servidor
 
+app.use(express.json())// Faça para o servidor qie vai receber dados json
 
-const database = []
+const database = [
+    {
+        id: 1,
+        title: "Desenvolvimento de sistemas",
+        curso: "Técnico",
+        turma: "3B",
+        professor: "Ramon Brignolli"
+    }
+]
+
 
 
 //Criar minhas rotas
-app.get('/teste', (req, res) => {
-    console.log("usuário acessou")
+app.get('/testes', (req, res) => {
+    res.status(200).send(database)
     res.send('Qualquer coisa')
 })
 
-app.post('/aulas', (req, res)=>{
-    console.log('criando algo')
-    res.send('Criando algo')
+app.get('/aulas/:id', (req, res) => {
+    console.log(req.params.id)
+})
+
+app.post('/aulas', (req, res)=> {
+    const dados = req.body
+    dados['id'] = database.length + 1
+
+    console.log(req.body)
+    res.send(database)
 })
 
 
